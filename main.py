2f31b3b1e653cd76c061fc13e731e9605ec33d88
ItsTato <ItsTato@gmail.com>
2021-12-20 19:47:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2021 ItsTato
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os,sys,platform,time,random
 from datetime import datetime
 
@@ -31,15 +55,6 @@ from tabulate import tabulate
 import psutil
 
 Side = "="*35
-
-#            os.system("cls" if (os.name == "nt") else "clear")           
-#            print(random.choice(loading))
-#            time.sleep(.25)
-#            # do thingies UwU
-#            os.system("cls" if (os.name == "nt") else "clear")
-#            # print thingies OwO
-#            time.sleep(.5)
-
 space = " "*25
 loading = [f"\n\n{space}Complete Resource Monitoring\n{space}🛫 Loading\n\n",f"\n\n{space}Complete Resource Monitoring\n{space}🛬 Loading\n\n",f"\n\n{space}Complete Resource Monitoring\n{space}👉👈 Loading\n\n"]
 useFreq = True
